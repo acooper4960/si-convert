35945fa68d980737ff3589dcd095929d1e3243ac
Alex <alex.cooper@docusign.com>
2018-11-20 00:15:12 -0800

    heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import http from 'http'
 import app from './server'
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const server = http.createServer(app)
 let currentApp = app
-server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+server.listen(process.env.PORT || PORT, () => console.log(`listening on port ${process.env.PORT || PORT}`));
 if (module.hot) {
   module.hot.accept('./server', () => {
     server.removeListener('request', currentApp)
