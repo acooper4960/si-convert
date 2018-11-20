65c48ed412b6ec99402adfb150b0b3fd088ead2b
Alex <alex.cooper@docusign.com>
2018-11-20 00:37:18 -0800

    / route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ import convert from './convert';
 
 const app = express();
 
+
+app.get('/', (req, res) => {
+  res.send('go to /units/si');
+});
+
+
 app.get('/units/si', (req, res) => {
     res.send({
         Value: convert(req.query.units)
